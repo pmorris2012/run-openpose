668616a93844c9183eff5fba9fba4f70b090aa14
Paul Morris <pmorris2012@fau.edu>
2020-11-05 00:47:10 -0500

    code formatting

--- run-openpose/cv_utils.py
+++ run-openpose/cv_utils.py
@@ -1,21 +1,24 @@
 import os
 import cv2
 
+
 def check_video(file):
     video = cv2.VideoCapture(file)
-    frames_remaining, frame = video.read()
+    frames_remaining, _ = video.read()
     return frames_remaining
+
 
 def check_image(file):
     return cv2.haveImageReader(file)
 
+
 def get_video_properties(video):
-    if type(video) == str: #if file path, load video
+    if type(video) == str:  # if file path, load video
         video = cv2.VideoCapture(video)
-        
+
     return {
-        'fps': video.get(cv2.CAP_PROP_FPS),
-        'width': int(video.get(cv2.CAP_PROP_FRAME_WIDTH)),
-        'height': int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)),
-        'frames': int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        "fps": video.get(cv2.CAP_PROP_FPS),
+        "width": int(video.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        "height": int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+        "frames": int(video.get(cv2.CAP_PROP_FRAME_COUNT)),
     }
--- run-openpose/file_utils.py
+++ run-openpose/file_utils.py
@@ -1,10 +1,13 @@
 import os
 
+
 def move_path(path, folder_from, folder_to):
     return path.replace(folder_from, folder_to, 1)
 
+
 def replace_ext(file, ext):
     return os.path.splitext(file)[0] + ext
+
 
 def create_dirs(path):
     directory = os.path.dirname(path)
